70a0152ed737c7911eb3e500bec9359ed0af8fd9
hamao0820 <hamashun0820@icloud.com>
2025-02-24 17:40:22 +0900

    add copyright and String()

--- geom.go
+++ geom.go
@@ -1,6 +1,21 @@
+// Copyright 2014 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package koebiten
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -17,6 +32,11 @@ type GeoM struct {
 	d_1 float64 // The actual 'd' value minus 1
 	tx  float64
 	ty  float64
+}
+
+// String returns a string representation of GeoM.
+func (g *GeoM) String() string {
+	return fmt.Sprintf("[[%f, %f, %f], [%f, %f, %f]]", g.a_1+1, g.b, g.tx, g.c, g.d_1+1, g.ty)
 }
 
 // Reset resets the GeoM as identity.
