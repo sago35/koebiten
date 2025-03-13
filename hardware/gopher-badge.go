6cac60066ffbe4734eb1dcce795f6aba4f35d6b9
sago35 <sago35@gmail.com>
2025-03-13 21:34:20 +0900

    Reduce heap allocs

--- hardware/gopher-badge.go
+++ hardware/gopher-badge.go
@@ -19,6 +19,7 @@ type device struct {
 	gpioPins []machine.Pin
 	state    []State
 	cycle    []int
+	keybuf   [1]koebiten.Key
 }
 
 const (
@@ -36,7 +37,7 @@ const (
 
 func (z *device) Init() error {
 	machine.SPI0.Configure(machine.SPIConfig{
-		Frequency: 32 * machine.MHz,
+		Frequency: 48 * machine.MHz,
 		Mode:      0,
 	})
 
@@ -99,6 +100,7 @@ func (z *device) GetDisplay() koebiten.Displayer {
 }
 
 func (z *device) KeyUpdate() error {
+	buf := z.keybuf[:]
 	for r := range z.gpioPins {
 		current := !z.gpioPins[r].Get()
 		if z.gpioPins[r] == machine.NoPin {
@@ -120,9 +122,11 @@ func (z *device) KeyUpdate() error {
 			}
 		case NoneToPress:
 			z.state[idx] = Press
-			koebiten.AppendJustPressedKeys([]koebiten.Key{koebiten.Key(idx)})
+			buf[0] = koebiten.Key(idx)
+			koebiten.AppendJustPressedKeys(buf)
 		case Press:
-			koebiten.AppendPressedKeys([]koebiten.Key{koebiten.Key(idx)})
+			buf[0] = koebiten.Key(idx)
+			koebiten.AppendPressedKeys(buf)
 			if current {
 				z.cycle[idx] = 0
 			} else {
@@ -135,7 +139,8 @@ func (z *device) KeyUpdate() error {
 			}
 		case PressToRelease:
 			z.state[idx] = None
-			koebiten.AppendJustReleasedKeys([]koebiten.Key{koebiten.Key(idx)})
+			buf[0] = koebiten.Key(idx)
+			koebiten.AppendJustReleasedKeys(buf)
 		}
 	}
 	return nil
